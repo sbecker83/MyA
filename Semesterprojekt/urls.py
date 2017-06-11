3be6fc17ec2c073abe55222fbeb71db3b0ec5a78
Matthias H <matthias.h@gmx.net>
2017-06-11 11:03:56 +0200

    Simple login with bootstrap and default django auth added

--- Semesterprojekt/urls.py
+++ Semesterprojekt/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.conf.urls import url
 from MyA.views import get_staff
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 admin.autodiscover()
 
 """ 
@@ -31,4 +33,6 @@ url(r'^staff/$', staff)
 urlpatterns = (
     url(r'^admin/', admin.site.urls),
     url(r'^staff/', get_staff),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 )
