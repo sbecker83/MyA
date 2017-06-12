66f98ef7564e9da327fc89b9e4d070bcc544bd45
sbecker83 <sandra.becker@stud.fh-luebeck.de>
2017-06-12 11:59:17 +0200

    Site newStaff with url, newStaff template

--- MyA/views.py
+++ MyA/views.py
@@ -1,12 +1,20 @@
+"""
+Beschreibung zum Inhalt der Dabei
+"""
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from MyA.models import Mitarbeiter
 
 # Create your views here.
 
-
+# Mitarbeiter - Hauptseite
 def get_staff(request):
-
     mitarbeiters = Mitarbeiter.objects.all()
+    return render(request, 'staff/staff.html', {'page_titel': 'Mitarbeiter', 'mitarbeiters':mitarbeiters})
 
-    return render(request, 'staff.html', {'page_titel': 'Mitarbeiter', 'mitarbeiters':mitarbeiters})
+# Neue Mitarbeiter Anlegen
+def new_staff(request):
+    return render(request, 'staff/newStaff.html',  {'page_titel': 'Neue Mitarbeiter anlegen'})
+
+
--- Semesterprojekt/urls.py
+++ Semesterprojekt/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.conf.urls import url
-from MyA.views import get_staff
+from MyA.views import get_staff, new_staff
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
@@ -32,7 +32,8 @@ url(r'^staff/$', staff)
 
 urlpatterns = (
     url(r'^admin/', admin.site.urls),
-    url(r'^staff/', get_staff),
+    url(r'^staff/$', get_staff),
+    url(r'^staff/newStaff/$', new_staff),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 )
