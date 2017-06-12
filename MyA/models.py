b5b8c1cb6a2e6a2e477ac7a96dcab348b86c651f
sbecker83 <sandra.becker@stud.fh-luebeck.de>
2017-06-12 22:09:11 +0200

    neu

--- MyA/models.py
+++ MyA/models.py
@@ -3,23 +3,22 @@ from datetime import datetime
 
 # Create your models here.
 
-
 # id = models.AutoField(primary_key=True)
 
-class Mitarbeiter(models.Model):
+class Staffs(models.Model):
     nickname = models.CharField('nickname', max_length=75)
     pwd = models.CharField('pwd', max_length=100)
-    vorname = models.CharField('vorname', max_length=100)
-    nachname = models.CharField('nachname', max_length=100)
-    ANREDE=(
-        ('K', 'Keine Auswahl'),
-        ('F', 'Frau'),
-        ('H', 'Herr'),
+    firstname = models.CharField('firstname', max_length=100)
+    lastname = models.CharField('lastname', max_length=100)
+    GENDER=(
+        ('N', 'No Select'),
+        ('F', 'Female'),
+        ('M', 'Male'),
     )
-    anrede = models.CharField('anrede', default='K', max_length=1, choices=ANREDE)
-    titel = models.CharField('titel', null=True, max_length=100)
+    gender = models.CharField('salutation', default='N', max_length=1, choices=GENDER)
+    title = models.CharField('title', null=True, max_length=100)
     position = models.CharField('position', max_length=100)
-    telefon = models.CharField('telefon', null=True, max_length=25)
+    phone = models.CharField('phone', null=True, max_length=25)
     fax = models.CharField('fax', null=True, max_length=100)
     mobile = models.CharField('mobile', null=True, max_length=100)
     email = models.CharField('email', null=True, max_length=100)
@@ -28,53 +27,53 @@ class Mitarbeiter(models.Model):
     #    return self.vorname, self.nachname, self.titel, self.position
 
     def __str__(self):
-        return "{} {} {} {}".format(self.vorname, self.nachname, self.titel, self.position)
+        return "{} {} {} {}".format(self.firstname, self.lastname, self.title, self.position)
 
-class Kunden(models.Model):
-    firma = models.CharField('firma', max_length=100)
-    strasse = models.CharField('strasse', null=True, max_length=100)
-    plzort = models.CharField('plzort', null=True, max_length=100)
-    telefon = models.CharField('telefon', null=True, max_length=100)
+class Customer(models.Model):
+    company = models.CharField('company', max_length=100)
+    street = models.CharField('street', null=True, max_length=100)
+    plzcity = models.CharField('plzcity', null=True, max_length=100)
+    phone = models.CharField('phone', null=True, max_length=100)
     fax = models.CharField('fax', null=True, max_length=100)
-    webseite = models.CharField('webseite', null=True, max_length=100)
+    website = models.CharField('website', null=True, max_length=100)
 
-class Ansprechpartner(models.Model):
-    kunden = models.ForeignKey(Kunden)
-    vorname = models.CharField('vorname', max_length=100)
-    nachname = models.CharField('nachname', max_length=100)
-    ANREDE=(
-        ('K', 'Keine Auswahl'),
-        ('F', 'Frau'),
-        ('H', 'Herr'),
+class Contacts(models.Model):
+    customer = models.ForeignKey(Customer)
+    firstname = models.CharField('firstname', max_length=100)
+    lastname = models.CharField('lastname', max_length=100)
+    GENDER=(
+        ('N', 'No Select'),
+        ('F', 'Female'),
+        ('M', 'Male'),
     )
-    anrede = models.CharField('anrede', default='K', max_length=1, choices=ANREDE)
-    titel = models.CharField('titel', null=True, max_length=100)
+    gender = models.CharField('gender', default='N', max_length=1, choices=GENDER)
+    title = models.CharField('title', null=True, max_length=100)
     position = models.CharField('position', max_length=100)
-    telefon = models.CharField('telefon', null=True, max_length=25)
+    phone = models.CharField('phone', null=True, max_length=25)
     fax = models.CharField('fax', null=True, max_length=100)
     mobile = models.CharField('mobile', null=True,  max_length=100)
     email = models.CharField('email', null=True, max_length=100)
 
-class Termine(models.Model):
-    mitarbeiter = models.ManyToManyField(Mitarbeiter, through='TeilnehmerInt')           #many to many Field
-    ansprechpartner = models.ManyToManyField(Ansprechpartner, through='TeilnehmerExt')   # many to many Field
-    datum = models.DateTimeField('datum', default=datetime.now())
-    titel = models.CharField('titel', max_length=100)
-    ort = models.CharField('ort', max_length=100)
+class Events(models.Model):
+    staff = models.ManyToManyField(Staffs, through='MemberInt')           #many to many Field
+    contact = models.ManyToManyField(Contacts, through='MemberExt')   # many to many Field
+    date = models.DateTimeField('date', default=datetime.now())
+    title = models.CharField('title', max_length=100)
+    location = models.CharField('location', max_length=100)
 
-class TeilnehmerExt(models.Model):
-    ansprechpartner = models.ForeignKey(Ansprechpartner)
-    termine = models.ForeignKey(Termine)
+class MemberExt(models.Model):
+    contact = models.ForeignKey(Contacts)
+    event = models.ForeignKey(Events)
     status = models.IntegerField('status', default=0)
 
-class TeilnehmerInt(models.Model):
-    mitarbeiter = models.ForeignKey(Mitarbeiter)
-    termine = models.ForeignKey(Termine)
-    leiter = models.BooleanField('leiter', default=False)
+class MemberInt(models.Model):
+    staff = models.ForeignKey(Staffs)
+    event = models.ForeignKey(Events)
+    leader = models.BooleanField('leader', default=False)
     status = models.IntegerField('status', default=0)
 
-class Notizen(models.Model):
-    ansprechpartner = models.ForeignKey(Ansprechpartner)
-    mitarbeiter = models.ForeignKey(Mitarbeiter)
-    gespraechsart = models.IntegerField('gespraechsart', default=0)
-    notiztext = models.CharField('nituztext', max_length=200)
+class Notes(models.Model):
+    contact = models.ForeignKey(Contacts)
+    staff = models.ForeignKey(Staffs)
+    calltype = models.IntegerField('calltype', default=0)
+    notetext = models.CharField('notetext', max_length=200)
