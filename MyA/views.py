ec95c3d125b2b281e18067d2c9025c1c87b38e56
RiRiSeffie <r.steffens@outlook.de>
2017-06-16 10:32:15 +0200

    Function for creating, changing and deleting for customers, contacts and notes, although the notes are not yet fully implemented

--- MyA/views.py
+++ MyA/views.py
@@ -65,7 +65,6 @@ def get_customer(request):
     customers = Customers.objects.all()
     return render(request, 'customer/customer.html', {'page_titel': 'Kunden', 'customers':customers})
 
-
 # create a new customer or edit a customer
 def details_customer(request, pk=None):
     if pk==None:
@@ -91,30 +90,114 @@ def details_customer(request, pk=None):
     else:
         # form first call
         form = CustomerForm (instance=customer)
-    return render(request, 'customer/detailsCustomer.html', {'page_titel': page_title, 'form':form})
+    return render(request, 'details.html', {'page_titel': page_title, 'form':form})
+
+# delete a customer
+def delete_customer(request, pk=None):
+    if pk==None:
+        messages.error (request, u'Daten konnten nicht gelöscht werden')
+    else:
+        customer = get_object_or_404(Customers,id=pk)
+        # check if customer has no contacts
+        nocontact= 0
+        for c in Contacts.objects.raw ('SELECT * FROM mya_contacts where customer_id='+pk):
+            nocontact = 1
+        if nocontact==0 :
+            customer.delete()
+            messages.success (request, u'Daten erfoglreich gelöscht')
+        else:
+            messages.error (request, u'Daten konnten nicht gelöscht werden')
+    return HttpResponseRedirect (reverse ('kundenliste'))
 
 # Contact - View
 def get_contact(request):
     contacts = Contacts.objects.all()
     return render(request, 'contact/contact.html', {'page_titel': 'Ansprechpartner', 'contacts':contacts})
 
-# Create new Customer
-def new_contact(request):
-    contact = Contacts ()
+# create a new contact or edit a contact
+def details_contact(request, pk=None):
+    if pk==None:
+        contact = Contacts ()
+        page_title="Ansprechpartner anlegen"
+    else:
+        contact = get_object_or_404(Contacts,id=pk)
+        page_title = "Ansprechpartner ändern"
+
     if request.method == 'POST':
 
-        #form sent off
-        form = ContactForm(request.POST,instance=contact)
-        #Validity check
-        if form.is_valid():
-            form.save()
+        # form sent off
+        form = ContactForm (request.POST, instance=contact)
+        # Validity check
+        if form.is_valid ():
+            form.save ()
             messages.success (request, u'Daten erfoglreich geändert')
-            return HttpResponseRedirect(reverse('ansprechpartnerliste'))
+            return HttpResponseRedirect (reverse ('ansprechpartnerliste'))
         else:
             # error message
             messages.error (request, u'Daten konnten nicht gespeichert werden')
             pass
     else:
         # form first call
-        form = ContactForm(instance=contact)
-    return render(request, 'contact/newcontact.html', {'page_titel': 'Ansprechpartner', 'form':form})
+        form = ContactForm (instance=contact)
+    return render(request, 'details.html', {'page_titel': page_title, 'form':form})
+
+# delete a contact
+def delete_contact(request, pk=None):
+    if pk==None:
+        messages.error (request, u'Daten konnten nicht gelöscht werden')
+    else:
+        contact = get_object_or_404 (Contacts, id=pk)
+        # check if contact has no notes and no events / memberext
+        no_notes_and_events= 0
+        for n in Notes.objects.raw ('SELECT * FROM mya_notes where contact_id='+pk):
+            no_notes_and_events = 1
+        for e in MemberExt.objects.raw ('SELECT * FROM mya_memberext where contact_id='+pk):
+            no_notes_and_events = 1
+        if no_notes_and_events==0 :
+            contact.delete()
+            messages.success (request, u'Daten erfoglreich gelöscht')
+        else:
+            messages.error (request, u'Daten konnten nicht gelöscht werden')
+    return HttpResponseRedirect (reverse ('ansprechpartnerliste'))
+
+# Note - View
+def get_notes(request):
+    notes = Notes.objects.all()
+    return render(request, 'note/note.html', {'page_titel': 'Notizen', 'notes':notes})
+
+# create a new note or edit a note
+def details_note(request, pk=None):
+    if pk==None:
+        note = Notes ()
+        page_title="Notiz anlegen"
+    else:
+        note = get_object_or_404(Notes,id=pk)
+        page_title = "Notiz ändern"
+
+    if request.method == 'POST':
+
+        #form sent off
+        form = NoteForm(request.POST,instance=note)
+        #Validity check
+        if form.is_valid():
+            form.save()
+            messages.success (request, u'Daten erfoglreich geändert')
+            return HttpResponseRedirect(reverse('kundenliste'))
+        else:
+            # error message
+            messages.error (request, u'Daten konnten nicht gespeichert werden')
+            pass
+    else:
+        # form first call
+        form = NoteForm (instance=note)
+    return render(request, 'details.html', {'page_titel': page_title, 'form':form})
+
+# delete a customer
+def delete_note(request, pk=None):
+    if pk==None:
+        messages.error (request, u'Daten konnten nicht gelöscht werden')
+    else:
+        note = get_object_or_404 (Notes, id=pk)
+        note.delete()
+
+    return HttpResponseRedirect (reverse ('notizliste'))
